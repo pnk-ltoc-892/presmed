5f32cbc3b2317d75ae5df0c2b307d9fc599fcc66
Pankaj Singh <165186164+pnk-ltoc-892@users.noreply.github.com>
2024-10-08 01:03:32 +0530

    Add Footer Component & Home Page Completed

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,25 @@ import MyProfile from './pages/MyProfile'
 import MyAppointments from './pages/MyAppointments'
 import Appointment from './pages/Appointment'
 import Navbar from './components/Navbar'
+import { Footer } from './components/Footer'
 
 export default function App() {
   return (
       <div className='bg-slate-900'>
           <div className='mx-4 sm:mx-[10%] bg-white'>
             <Navbar />
-            <Routes>
-              <Route path='/' element={<Home/>}/>
-              <Route path='/doctors' element={<Doctors/>}/>
-              <Route path='/doctors:speciality' element={<Doctors/>}/>
-              <Route path='/login' element={<Login/>}/>
-              <Route path='/about' element={<About/>}/>
-              <Route path='/contact' element={<Contact/>}/>
-              <Route path='/my-profile' element={<MyProfile/>}/>
-              <Route path='/appointments/:doctorid' element={<Appointment/>}/>
-            </Routes>
+              <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/doctors' element={<Doctors/>}/>
+                <Route path='/doctors:speciality' element={<Doctors/>}/>
+                <Route path='/login' element={<Login/>}/>
+                <Route path='/about' element={<About/>}/>
+                <Route path='/contact' element={<Contact/>}/>
+                <Route path='/my-profile' element={<MyProfile/>}/>
+                <Route path='/appointments/:doctorid' element={<Appointment/>}/>
+              </Routes>
+            <Footer />
           </div>
       </div>
   )
 }
-
-
