b61ffcfbb39554435f71bbfe6910202f853df41e
Pankaj Singh <165186164+pnk-ltoc-892@users.noreply.github.com>
2024-10-07 23:06:49 +0530

    Add Speciality Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Header from '../components/Header'
+import SpecialityMenu from '../components/SpecialityMenu'
 
 function Home() {
     return (
-        <div className='border-b-[1px] border-black '>
-            <Header />
-        </div>
+        <>
+            <div className='border-b-[1px] border-black '>
+                <Header />
+            </div>
+            <div className='border-b-[1px] border-black '>
+                <SpecialityMenu />
+            </div>
+        </>
     )
 }
 
