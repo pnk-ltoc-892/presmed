1b0a9c2d6435d1252e270304a51a857a635d4072
Pankaj Singh <165186164+pnk-ltoc-892@users.noreply.github.com>
2024-10-10 01:48:53 +0530

    Some Responsive Fixes

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -20,7 +20,7 @@ function MyProfile() {
 
 
     return (
-        <div className='m-12 max-w-lg flex flex-col gap-4 text-sm'>
+        <div className='max-w-lg flex flex-col gap-4 text-sm'>
             <div className='flex gap-5'>
                 <img src={userData.image} alt="" className='w-36 rounded ml-4'/>
                 <input type="file" className='flex items-center'/>
@@ -40,34 +40,34 @@ function MyProfile() {
                     <p className='text-neutral-500 underline mt-3 pl-2'>CONTACT INFORMATION</p>
                     
                     <div className='pt-2'>
-                        <label className="w-1/4 inline-block m-2 text-lg font-medium text-gray-900">Email:</label>
+                        <label className="w-1/4 inline-block m-2 text-sm md:text-lg font-medium text-gray-900">Email:</label>
                         <input  type="text" 
-                                className={`text-primary text-lg px-4 py-1 ${isEdit === true ? 'bg-gray-100 border border-gray-400 rounded-lg' : ''}`} 
+                                className={`text-primary text-sm md:text-lg px-4 py-1 ${isEdit === true ? 'bg-gray-100 border border-gray-400 rounded-lg' : ''}`} 
                                 disabled={!isEdit}
                                 value={userData.email} 
                                 onChange={(e) => setUserData({...userData, email: e.target.value})}
                                 />      
                     </div>
                     <div className='pt-2'>
-                        <label className="w-1/4 inline-block m-2 text-lg font-medium text-gray-900 ">Phone:</label>
+                        <label className="w-1/4 inline-block m-2 text-sm md:text-lg font-medium text-gray-900 ">Phone:</label>
                         <input  type="text" 
-                                className={`text-primary text-lg px-4 py-1 ${isEdit === true ? 'bg-gray-100 border border-gray-400 rounded-lg' : ''}`} 
+                                className={`text-primary text-sm md:text-lg px-4 py-1 ${isEdit === true ? 'bg-gray-100 border border-gray-400 rounded-lg' : ''}`} 
                                 disabled={!isEdit}
                                 value={userData.phone} 
                                 onChange={(e) => setUserData({...userData, phone: e.target.value})}
                                 />      
                     </div>
                     <div className='flex pt-2'>
-                        <label className="w-1/4 inline-block m-2 text-lg font-medium text-gray-900 ">Address:</label>
+                        <label className="w-1/4 inline-block m-2 text-sm md:text-lg font-medium text-gray-900 ">Address:</label>
                         <div className='flex flex-col'>
                         <input  type="text" 
-                                className={`text-gray-700 text-lg px-4 py-1 mt-1 ${isEdit === true ? 'bg-gray-100 border border-gray-400 rounded-lg' : ''}`} 
+                                className={`text-gray-700 text-sm md:text-lg px-4 py-1 mt-1 ${isEdit === true ? 'bg-gray-100 border border-gray-400 rounded-lg' : ''}`} 
                                 disabled={!isEdit}
                                 value={userData.address.line1} 
                                 onChange={(e) => setUserData({...userData, address: {...userData.address, line1: e.target.value}})}
                                 />      
                         <input  type="text" 
-                                className={`text-gray-700 text-lg px-4 mt-1 py-1 ${isEdit === true ? 'bg-gray-100 border border-gray-400 rounded-lg' : ''}`} 
+                                className={`text-gray-700 text-sm md:text-lg px-4 mt-1 py-1 ${isEdit === true ? 'bg-gray-100 border border-gray-400 rounded-lg' : ''}`} 
                                 disabled={!isEdit}
                                 value={userData.address.line2} 
                                 onChange={(e) => setUserData({...userData, address: {...userData.address, line2: e.target.value}})}
@@ -77,22 +77,22 @@ function MyProfile() {
 
                     <p className='text-neutral-500 underline mt-3 pl-2'>BASIC INFORMATION</p>
                     <div  className='pt-2'>
-                        <label className="w-1/4 inline-block m-2 text-lg font-medium text-gray-900 ">Gender: </label>
+                        <label className="w-1/4 inline-block m-2 text-sm md:text-lg font-medium text-gray-900 ">Gender: </label>
                         <select 
-                            className={`text-gray-700 text-lg px-4 py-1 ${isEdit === true ? 'bg-gray-100 border border-gray-400 rounded-lg' : ''}`}
+                            className={`text-gray-700 text-sm md:text-lg px-4 py-1 ${isEdit === true ? 'bg-gray-100 border border-gray-400 rounded-lg' : ''}`}
                             onChange={(e) => {setUserData({...userData, gender: e.target.value});console.log(e.target.value);
                             }}
                             value={userData.gender}
                             disabled={!isEdit}
                             >
-                            <option value="Male" className='text-gray-700 text-lg'>Male</option>
-                            <option value="Female" className='text-gray-700 text-lg'>Female</option>
+                            <option value="Male" className='text-gray-700 text-sm'md:text-lg >Male</option>
+                            <option value="Female" className='text-gray-700 text-sm'md:text-lg >Female</option>
                         </select>
                     </div>
                     <div  className='pt-2'>
-                        <label className="w-1/4 inline-block m-2 text-lg font-medium text-gray-900 ">D.O.B: </label>
+                        <label className="w-1/4 inline-block m-2 text-sm md:text-lg font-medium text-gray-900 ">D.O.B: </label>
                         <input  type="date" 
-                                className={`text-gray-700 text-lg px-4 py-1 ${isEdit === true ? 'bg-gray-100 border border-gray-400 rounded-lg' : ''}`} 
+                                className={`text-gray-700 text-sm md:text-lg px-4 py-1 ${isEdit === true ? 'bg-gray-100 border border-gray-400 rounded-lg' : ''}`} 
                                 disabled={!isEdit} 
                                 value={userData.dob} 
                                 onChange={(e) => {setUserData({...userData, dob: e.target.value}); console.log(e.target.value);
